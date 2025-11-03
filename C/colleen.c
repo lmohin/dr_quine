241a7e43e99231757859e2a6d009b38ea4d9a4ba
lmohin <loic.mohin@gmail.com>
2025-11-03 16:50:54 +0100

    renaming file and removing \r

---
+++ C/colleen.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main(void){char *to_print = "#include <stdio.h>%c%cint main(void){char *to_print = %c%s%c;printf(to_print, 10, 10, 34, to_print, 34, 10);}%c";printf(to_print, 10, 10, 34, to_print, 34, 10);}
