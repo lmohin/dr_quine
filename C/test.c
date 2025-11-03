60a30732b4cd5b972fb3cfa59323387a89b0df62
lmohin <loic.mohin@gmail.com>
2025-11-03 16:46:24 +0100

    creating correct folders

---
+++ C/test.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main(void){char *to_print = "#include <stdio.h>%c%c%c%cint main(void){char *to_print = %c%s%c;printf(to_print, 13, 10, 13, 10, 34, to_print, 34, 13, 10);}%c%c";printf(to_print, 13, 10, 13, 10, 34, to_print, 34, 13, 10);}
