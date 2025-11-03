e22e65c9ab26a78328d15c52b167227001614aba
lmohin <loic.mohin@gmail.com>
2025-11-03 17:07:47 +0100

    adding comms and function

--- C/colleen.c
+++ C/colleen.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 
-int main(void){char *to_print = "#include <stdio.h>%c%cint main(void){char *to_print = %c%s%c;printf(to_print, 10, 10, 34, to_print, 34, 10);}%c";printf(to_print, 10, 10, 34, to_print, 34, 10);}
+void useless(void){return;}
+
+/*Comment present outside of the program*/
+
+int main(void){/*Comment present in the main function*/useless(); char *to_print = "#include <stdio.h>%c%cvoid useless(void){return;}%c%c/*Comment present outside of the program*/%c%cint main(void){/*Comment present in the main function*/useless(); char *to_print = %c%s%c;printf(to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 10);}%c";printf(to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 10);}
