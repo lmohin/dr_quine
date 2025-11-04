0c4ac81700dd6472660cdaff82ae0df49dfc67fe
lmohin <loic.mohin@gmail.com>
2025-11-04 19:35:09 +0100

    now im using fopen

--- C/grace.c
+++ C/grace.c
@@ -4,6 +4,6 @@
   This is a useless comment
 */
 
-#define FT()int main(){char *to_print="#include <unistd.h>%c%c/*%c  This is a useless comment%c*/%c%c#define FT()int main(){char *to_print=%c%s%c; printf(to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 10, 10);}%c%cFT()"; printf(to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 10, 10);}
+#define FT()int main(){char *to_print="#include <unistd.h>%c%c/*%c  This is a useless comment%c*/%c%c#define FT()int main(){char *to_print=%c%s%c; FILE *f = fopen(%cGrace_kid.c%c, %cw%c); fprintf(f, to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 34, 34, 34, 34, 10, 10);}%c%cFT()"; FILE *f = fopen("Grace_kid.c", "w"); fprintf(f, to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 34, 34, 34, 34, 10, 10);}
 
 FT()
