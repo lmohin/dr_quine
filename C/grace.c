cea17fb07b05b43a832316229752daa426fef730
lmohin <loic.mohin@gmail.com>
2025-11-04 19:14:52 +0100

    adding a comment to respect subject

--- C/grace.c
+++ C/grace.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
-#define FT()int main(){char *to_print="#include<unistd.h>%c%c#define FT(x)int main(){char *to_print=%c%s%c; printf(to_print, 10, 10, 34, to_print, 34, 10, 10);}%c%cFT()"; printf(to_print, 10, 10, 34, to_print, 34, 10, 10);}
+/*
+  This is a useless comment
+*/
+
+#define FT()int main(){char *to_print="#include <unistd.h>%c%c/*%c  This is a useless comment%c*/%c%c#define FT()int main(){char *to_print=%c%s%c; printf(to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 10, 10);}%c%cFT()"; printf(to_print, 10, 10, 10, 10, 10, 10, 34, to_print, 34, 10, 10);}
 
 FT()
